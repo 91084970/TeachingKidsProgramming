e910191096c37be309678b91d15dc2152aa4befe
91078024 <31664275+91084970@users.noreply.github.com>
2017-11-02 14:32:17 -0700

    worked on DigiFlower

--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -10,33 +10,33 @@ public class PentagonCrazy
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //    createColorPalette (recipe below) --#8.1
-    // 
-    //    drawPentagon (recipe below) --#10.1
-    //
+    drawPentagon();
+  }
+  private static void drawPentagon()
+  {
     //    ------------- Recipe for drawPentagon --#10.2
     for (int i = 0; i < 200; i++)
     {
-      //    Do the following 200 times --#2.1
       adjustPen();
       int length = i;
       Tortoise.move(length);
       Tortoise.turn(360 / 5);
       Tortoise.turn(1);
     }
-    //        Turn the tortoise 1 more degree --#5
-    //    End Repeat --#2.2
-    //    ------------- End of drawPentagon recipe --#10.3
   }
   private static void adjustPen()
   {
-    //    ------------- Recipe for createColorPalette --#8.2
-    //    Add steel blue to the color wheel --#7
+   Tortoise.setPenColor(ColorWheel.getNextColor());
+   Tortoise.setPenWidth(Tortoise.getPenWidth()+1); 
+   if (Tortoise.getPenWidth()>4)
+  }
+}Private
+  static void createColorPalette()
+  {
     ColorWheel.addColor(PenColors.Blues.SteelBlue);
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
-    //    ------------- End of createColorPalette recipe --#8.3
+    ColorWheel.addColor(PenColors.Purples.DarkOrchid);
+    ColorWheel.addColor(PenColors.Blues.DarkSlateBlue);
+    ColorWheel.addColor(PenColors.Blues.Teal);
+    ColorWheel.addColor(PenColors.Purples.Indigo);
   }
 }
